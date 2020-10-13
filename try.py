036253792d97951c6f79dcbb7472829eea7fc972
ymamidi <ymamidi@uno.edu>
2020-10-13 12:14:28 -0500

    text box created

--- try.py
+++ try.py
@@ -3,3 +3,15 @@ import pandas as pd
 from datetime import datetime
 
 st.title("Tissue Biorepository")
+
+df = pd.read_csv (r'C:\Personal Projects\Development\Tarun\Tissue_data.csv',  encoding= 'unicode_escape')
+
+#selecting Chromosome
+st.sidebar.subheader("Chromosome")
+Chromo_list = st.sidebar.text_input(label="", value="", key="na_upper")
+# df = (df.loc[df['Type']== Type_list])
+
+#selecting position
+st.sidebar.subheader("Position")
+Position_list = st.sidebar.text_input(label="", value="", key="na_lower")
+# df = (df.loc[df['Type']== Type_list])
